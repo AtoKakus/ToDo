f23ba6e8680b37bb11b47a8111d32a182c7bea0c
AtoKakus <bartoszzawiercie@gmail.com>
2022-06-11 18:54:55 +0200

    the end

--- js/main.js
+++ js/main.js
@@ -8,65 +8,79 @@ const btnClose = document.querySelector(".close")
 const inputTask = document.querySelector(".task")
 const inputEditTask = document.querySelector(".edit-task")
 
-const editBody = document.querySelector('.edit-body')
+const editBody = document.querySelector(".edit-body")
 const tasksBox = document.querySelector(".tasks-box")
 const error = document.querySelector(".error")
+const errorEdit = document.querySelector('.error-edit')
 
 let newTask
 let id
+let y
+
 
 const checkTask = () => {
 	if (inputTask.value !== "") {
 		error.style.opacity = "0"
 		addTask()
-        inputTask.value=''
+		inputTask.value = ""
 	} else {
 		error.style.opacity = "1"
 	}
 }
 
-const addTask=()=>{
-    newTask = document.createElement('div')
-    newTask.classList.add('task')
-    newTask.innerHTML=`
+const addTask = () => {
+	newTask = document.createElement("div")
+	newTask.classList.add("task")
+	newTask.innerHTML = `
     <p class="content-task">${inputTask.value}</p>
 							<div class="box-buttons">
 								<button class="ok btntask"><i class="fa-solid fa-check"></i></button>
 								<button class="edit btntask"><p class="edit">EDIT</p></button>
 								<button class="close btntask"><i class="fa-solid fa-x"></i></button>
 							</div>`
-    tasksBox.append(newTask)
+	tasksBox.append(newTask)
 }
 
-const checkClick = (e)=>{
-  
-    if(e.target.matches('.fa-check')){
-       const l = e.target.closest('.task')
-       console.log(l.firstChild);
-    }
-    else if (e.target.matches('.edit')){
-    showEditBody()
-      editTask(e)  
-    }
+const checkClick = (e) => {
+	if (e.target.matches(".fa-check")) {
+		const l = e.target.closest(".task")
+		const w = l.children[0]
+		w.classList.toggle("completed")
+	} else if (e.target.matches(".edit")) {
+        const x=e.target.closest(".task")
+        y = x.children[0]
+		showEditBody()
+		
+	} else if (e.target.matches(".fa-x")) {
+		e.target.closest(".task").remove()
+		closeEditBody()
+	}
+}
+const checkEditInput = ()=>{
+ if(inputEditTask.value!==''){
+    editTask()
+ }
+ else{
+errorEdit.style.opacity='1'
+    
+ }
 
-    else if(e.target.matches('.fa-x')){
-        e.target.closest('.task').remove()
-        closeEditBody()
-    }
+}
+const editTask = (e) => {
+    y.textContent=inputEditTask.value
+    inputEditTask.value=''
+   closeEditBody()
+   errorEdit.style.opacity='0'
 }
 
-const editTask = (et)=>{
-   
-console.log(sie);
+const showEditBody = () => {
+	editBody.style.display = "flex"
 }
-
-const showEditBody = ()=>{
-    editBody.style.display='flex'
-}
-const closeEditBody = ()=>{
-    editBody.style.display='none'
+const closeEditBody = () => {
+	editBody.style.display = "none"
 }
 
 tasksBox.addEventListener("click", checkClick)
 btnAdd.addEventListener("click", checkTask)
-btnCancel.addEventListener('click', closeEditBody)
+btnCancel.addEventListener("click", closeEditBody)
+btnSave.addEventListener("click", checkEditInput)
