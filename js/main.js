cfc0d329dc72313923643f0b9412834ac45db4f7
AtoKakus <bartoszzawiercie@gmail.com>
2022-06-11 16:35:48 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const btnAdd = document.querySelector(".add-task")
+const btnSave = document.querySelector(".save")
+const btnCancel = document.querySelector(".cancel")
+const btnOk = document.querySelector(".ok")
+const btnEdit = document.querySelector(".edit")
+const btnClose = document.querySelector(".close")
+
+const inputTask = document.querySelector(".task")
+const inputEditTask = document.querySelector(".edit-task")
+
+const editBody = document.querySelector('.edit-body')
+const tasksBox = document.querySelector(".tasks-box")
+const error = document.querySelector(".error")
+
+let newTask
+let id
+
+const checkTask = () => {
+	if (inputTask.value !== "") {
+		error.style.opacity = "0"
+		addTask()
+        inputTask.value=''
+	} else {
+		error.style.opacity = "1"
+	}
+}
+
+const addTask=()=>{
+    newTask = document.createElement('div')
+    newTask.classList.add('task')
+    newTask.innerHTML=`
+    <p class="content-task">${inputTask.value}</p>
+							<div class="box-buttons">
+								<button class="ok btntask"><i class="fa-solid fa-check"></i></button>
+								<button class="edit btntask"><p class="edit">EDIT</p></button>
+								<button class="close btntask"><i class="fa-solid fa-x"></i></button>
+							</div>`
+    tasksBox.append(newTask)
+}
+
+const checkClick = (e)=>{
+  
+    if(e.target.matches('.fa-check')){
+       const l = e.target.closest('.task')
+       console.log(l.firstChild);
+    }
+    else if (e.target.matches('.edit')){
+    showEditBody()
+      editTask(e)  
+    }
+
+    else if(e.target.matches('.fa-x')){
+        e.target.closest('.task').remove()
+        closeEditBody()
+    }
+}
+
+const editTask = (et)=>{
+   
+console.log(sie);
+}
+
+const showEditBody = ()=>{
+    editBody.style.display='flex'
+}
+const closeEditBody = ()=>{
+    editBody.style.display='none'
+}
+
+tasksBox.addEventListener("click", checkClick)
+btnAdd.addEventListener("click", checkTask)
+btnCancel.addEventListener('click', closeEditBody)
